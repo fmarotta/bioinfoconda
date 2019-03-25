ba1f0e9ffda28bb08c20110fca62045a3490ba48
Federico Marotta <federicomarotta@mail.com>
2019-03-25 13:32:03 +0100

    X-getdata: support rsync downloads

--- bioinfoconda/local/src/X-getdata.pl
+++ bioinfoconda/local/src/X-getdata.pl
@@ -15,40 +15,52 @@ use Getopt::Long;
 
 # TODO: accept patterns for http too.
 
-# TODO: check if new dest is overriding something
+# TODO: accept patterns in the middle of the path.
 
-# TODO: support file:// and scp:// protocols for local downloads
+# TODO: check if new dest is overriding something.
+
+# TODO: pass @ARGV as @params for rsync, e.g. to specify port or
+# something else in the command line.
 
 my $NAME = basename($0);
 my $USAGE =<<USAGE;
 Usage: $NAME [options] URL
+   or: $NAME [options] USER\@HOST:PATH
 
 Options:
-        -R|--reject=pattern     Reject files matching pattern. (Not yet implemented)
-        -f|--fix-index          Attempt to interactively fix the data index
-        -h|--help       		Print this message.
+	-R|--reject=pattern		Reject files matching pattern. (Not yet implemented)
+	-f|--fix-index			Attempt to interactively fix the data index
+	-h|--help				Print this message.
 
 Notes:
 	URL is the Universal Resource Location of the file to download.
 	$NAME can use either the ftp(s) or the http(s) protocol. In the former
 	case, URLs can contain patterns (such as [], {}, ? and * elements), so
-	that all the files matching the pattern are downloaded.
+	that all the files matching the pattern are downloaded; the pattern
+	can be specified only for the filename, not for dirnames.
+
+	If the file is on a remote host (or even in a given location of the local
+	host, use the second syntax, which is similar to the one used by scp
+	and rsync, except that you don't have to provide a destination. Like URL,
+	PATH can contain patterns, but only in the filename, not in the dirname.
 
 	The script automatically suggests a possible location to save the
 	downloads: for instance, if the URL is http://foo.com/boo/bar/baz.vcf.gz,
 	the suggestion will be foo/boo/bar/baz.vcf.gz. You will then be prompted
-	for a new location, where you can provide an alternative path.
+	for a new location, where you can provide an alternative path. The
+	suggestions for PATH are somewhat less sophisticated and in 100% of the
+	cases you will want to manually override them.
 
-	If the URL ends with a trailing /, it is interpreted as /*.
-	The destination path must be relative to the bioinfo data directory, and
-        if it does not exist, then it is created. When the protocol is not
-        specified, it is supposed to be http.
+	If the URL or the remothe PATH end with a trailing /, it is
+	interpreted as /*. The destination path must be relative to the bioinfo
+	data directory, and if it does not exist, then it is created. When the
+	protocol is not specified, it is supposed to be http.
 
-        Each time a file is downloaded, an entry to the index is 
-        appended so that the user who downloaded the file, the time of the 
-        download, the url and the local path are logged. If some files are moved
-        or downloaded manually, the index might become unrepresentative, therefore
-        it has to be fixed manually.
+	Each time a file is downloaded, an entry to the index is 
+	appended so that the user who downloaded the file, the time of the 
+	download, the url and the local path are logged. If some files are moved
+	or downloaded manually, the index might become unrepresentative, therefore
+	it has to be fixed manually.
 
 Reporting bugs:
 	federicomarotta AT mail DOT com
@@ -110,37 +122,73 @@ if ($fix_index)
 # Parse arguments
 if (scalar(@ARGV) != 1)
 {
-        print $USAGE;
-        exit 1;
+	print $USAGE;
+	exit 1;
 }
-my $url = $ARGV[0];
-
-# Parse the URL
-my ($protocol, $domain, $port, $path, @params) = parse_url($url);
-
-# Validate the URL
-my $v_url = validate_url($protocol, $domain, $port, $path, \@params);
-
-# Find an appropriate destination path
-my $dest = find_dest($domain, $path, \@params);
-print "\nSaving files to $data_root$dest.\n\n";
-
-# Finally download the files
+my $source = $ARGV[0];
 my @output = ();
-if (is_recursive($path))
+
+# Recognise the protocol
+if ($source =~ m#^https?://# or $source =~ m#^ftps?://#)
 {
-        @output = wget_recursive($v_url, $data_root . $dest);
+	# Parse the URL
+	my ($protocol, $domain, $port, $path, @params) = parse_url($source);
+	
+	# Validate the URL
+	my $v_source = validate_url($protocol, $domain, $port, $path, \@params);
+
+	# Find an appropriate destination path
+	my $dest = find_dest_url($domain, $path, \@params);
+
+	# Ensure that dest ends with a / for recursive downloads
+	if (is_recursive($v_source) and $dest !~ m/\/$/) {
+		$dest .= "/";
+	}
+	print "\nSaving files to $data_root$dest.\n\n";
+	
+	# Finally download the files
+	if (is_recursive($v_source))
+	{
+	        @output = wget_recursive($v_source, $data_root . $dest);
+	}
+	else
+	{
+	        @output = wget_simple($v_source, $data_root . $dest);
+	}
 }
-else
+elsif ($source =~ m#^rsync://# or $source =~ m/@/)
 {
-        @output = wget_simple($v_url, $data_root . $dest);
+	# Parse the hostname
+	my ($protocol, $user, $host, $path) = parse_host($source);
+
+	# Validate the hostname
+	my $v_source = validate_host($protocol, $user, $host, $path);
+
+	# Get an appropriate destination path
+	my $dest = find_dest_host($host, $path);
+
+	# Ensure that dest ends with a / for recursive downloads
+	if (is_recursive($v_source) and $dest !~ m/\/$/) {
+		$dest .= "/";
+	}
+	print "\nSaving files to $data_root$dest.\n\n";
+
+	# Finally download the files
+	if (is_recursive($v_source))
+	{
+		@output = rsync_recursive($v_source, $data_root . $dest);
+	}
+	else
+	{
+		@output = rsync_simple($v_source, $data_root . $dest);
+	}
 }
 
 # Add an entry to the index file
 if (@output)
 {
-        print "Updating index...\n";
-        update_index(\@output);
+	print "Updating index...\n";
+	update_index(\@output);
 }
 
 exit 0;
@@ -149,143 +197,231 @@ exit 0;
 
 sub parse_url
 {
-        my $url = $_[0];
+	my $url = $_[0];
 
-        my @parsed_url = ($url =~ m#^((.*)://)?(.*?)(:(\d+))?(/.*?)(\?(.+))?$#);
+	my @parsed_url = ($url =~ m#^((.*)://)?(.*?)(:(\d+))?(/.*?)(\?(.+))?$#);
 	my @path = split(//, $parsed_url[5]);
 
-        for (my $i = 0; $i < scalar(@parsed_url); $i++)
-        {
-                if (! $parsed_url[$i])
-                {
-                        $parsed_url[$i] = '';
-                }
-        }
-        if (! $parsed_url[1])
-        {
-                $parsed_url[1] = "http";
-        }
+	for (my $i = 0; $i < scalar(@parsed_url); $i++)
+	{
+		if (! $parsed_url[$i])
+		{
+			$parsed_url[$i] = '';
+		}
+	}
+	if (! $parsed_url[1])
+	{
+		$parsed_url[1] = "http";
+	}
+
 	# If url has a trailing slash, download recursively
 	if ($path[-1] eq '/')
 	{
 		$parsed_url[5] .= '*';
 	}
-        my @params = split(/&/, $parsed_url[6]);
+	my @params = split(/&/, $parsed_url[6]);
 
-        # The fields are as follows: protocol, domain name, port, path, query parameters
-        return ($parsed_url[1], $parsed_url[2], $parsed_url[4], $parsed_url[5], @params);
+	# The fields are as follows: protocol, domain name, port, path, query parameters
+	return ($parsed_url[1], $parsed_url[2], $parsed_url[4], $parsed_url[5], @params);
+}
+
+sub parse_host
+{
+	my $host = $_[0];
+
+	my @parsed_host = ($host =~ m#^((.*)://)?((.*)@)?(.*?)(:(.*))$#);
+	my @path = split(//, $parsed_host[6]);
+
+	for (my $i = 0; $i < scalar(@parsed_host); $i++)
+	{
+		if (! $parsed_host[$i])
+		{
+			$parsed_host[$i] = '';
+		}
+	}
+
+	# If the path has a trailing slash, download recursively
+	if ($path[-1] eq '/')
+	{
+		$parsed_host[6] .= '*';
+	}
+
+	# The fields are as follows: username, hostname, path
+	return ($parsed_host[1], $parsed_host[3], $parsed_host[4], $parsed_host[6]);
 }
 
 sub validate_url
 {
-        my $protocol = $_[0];
-        my $domain = $_[1];
-        my $port = $_[2];
-        my $path = $_[3];
-        my @params = @{$_[4]};
-        my $v_url = '';
-
-        # TODO: other validations
-
-        if ($protocol =~ m/^http/)
-        {
-                # Path cannot contain patterns
-                if ($path =~ m/[\*\?\[\]\{\}]/)
-                {
-                        die("ERROR: path cannot contain patterns if the protocol is http.\n");
-                }
-        }
-        elsif ($protocol =~ m/^ftp/)
-        {
+	my $protocol = $_[0];
+	my $domain = $_[1];
+	my $port = $_[2];
+	my $path = $_[3];
+	my @params = @{$_[4]};
+	my $v_url = '';
+	
+	# TODO: other validations
+	
+	if ($protocol =~ m/^http/)
+	{
+		# Path cannot contain patterns
+		if ($path =~ m/[\*\?\[\]\{\}]/)
+		{
+			die("ERROR: path cannot contain patterns if the protocol is http.\n");
+		}
+	}
+	elsif ($protocol =~ m/^ftp/)
+	{
 		# Only the last item can contain patterns
 		my @path = split(/\//, $path);
 		for (my $i = 0; $i < scalar(@path); $i++)
 		{
-                	if ($path[$i] =~ m/[\*\?\[\]\{\}]/ and $i != $#path)
+			if ($path[$i] =~ m/[\*\?\[\]\{\}]/ and $i != $#path)
 			{
 				die("ERROR: metacharacters are allowed only for file names, not for directories.\n");
 			}
 		}
-        }
-        else
-        {
-                die("ERROR: protocol $protocol is not supported.\n");
-        }
-
-        # Recompose the URL
-        $v_url = $protocol . "://" . $domain;
-        if ($port)
-        {
-                $v_url .= ":" . $port;
-        }
-        $v_url .= $path;
-        if (@params)
-        {
-                $v_url .= "?" . join('&', @params);
-        }
-
-        return $v_url;
+	}
+	else
+	{
+		die("ERROR: protocol $protocol is not supported.\n");
+	}
+	
+	# Recompose the URL
+	$v_url = $protocol . "://" . $domain;
+	if ($port)
+	{
+		$v_url .= ":" . $port;
+	}
+	$v_url .= $path;
+	if (@params)
+	{
+		$v_url .= "?" . join('&', @params);
+	}
+	
+	return $v_url;
 }
 
-sub find_dest
+sub validate_host
 {
-        my $domain = $_[0];
-        my $path = $_[1];
-        my @params = @{$_[2]};
+	my $protocol = $_[0];
+	my $username = $_[1];
+	my $hostname = $_[2];
+	my $path = $_[3];
+	my $v_host = '';
+
+	if ($username eq '')
+	{
+		$username = $ENV{USERNAME};
+	}
+	if ($path eq '')
+	{
+		die("ERROR: please provide the path of the file.\n");
+	}
+
+	# Only the last item can contain patterns
+	my @path = split(/\//, $path);
+	for (my $i = 0; $i < scalar(@path); $i++)
+	{
+		if ($path[$i] =~ m/[\*\?\[\]\{\}]/ and $i != $#path)
+		{
+			die("ERROR: metacharacters are allowed only for file names, not for directories.\n");
+		}
+	}
+
+	if ($protocol)
+	{
+		$v_host .= $protocol . "://";
+	}
+
+	$v_host .=  $username . "@" . $hostname . ":" . $path;
+	return $v_host;
+}
+
+sub find_dest_url
+{
+	my $domain = $_[0];
+	my $path = $_[1];
+	my @params = @{$_[2]};
 
 	# Extract the first meaningful level domain
-        my @levels = split(/\./, $domain);
-        my $fld = '';
-        for (my $i = 0; $i < scalar(@levels); $i++)
-        {
-                if (is_prefix($levels[$i]) || is_pseudo($levels[$i]))
-                {
-                        next;
-                }
-                else
-                {
-                        $fld = $levels[$i];
-                        last;
-                }
-        }
-
-        # Extract a path
-        my @dirs = split(/\//, $path);
-        my $localdirs = '';
-        for (my $i = 1; $i < scalar(@dirs); $i++)
-        {
-                if (is_pseudo($dirs[$i]))
-                {
-                        next;
-                }
-		elsif ($dirs[$i] =~ m/[\*\?\[\]\{\}]/)
+	my @levels = split(/\./, $domain);
+	my $fld = '';
+	for (my $i = 0; $i < scalar(@levels); $i++)
+	{
+		if (is_prefix($levels[$i]) || is_pseudo($levels[$i]))
 		{
-                        $localdirs .= '/';
+			next;
+		}
+		else
+		{
+			$fld = $levels[$i];
 			last;
 		}
-                else
-                {
-                        $localdirs .= '/' . $dirs[$i];
-                }
-        }
+	}
 
-        # Add params
-        if (@params)
-        {
-                for (my $i = 0; $i < scalar(@params); $i++)
-                {
-                        my @value = split(/=/, $params[$i]);
-                        $localdirs .= '/' . $value[1];
-                }
-        }
+	# Extract a path
+	my @dirs = split(/\//, $path);
+	my $localdirs = '';
+	for (my $i = 1; $i < scalar(@dirs); $i++)
+	{
+		if (is_pseudo($dirs[$i]))
+		{
+			next;
+		}
+		elsif ($dirs[$i] =~ m/[\*\?\[\]\{\}]/)
+		{
+			$localdirs .= '/';
+			last;
+		}
+		else
+		{
+			$localdirs .= '/' . $dirs[$i];
+		}
+	}
 
-        # Recompose the destination path
-        my $tmp_dest = $fld . $localdirs;
+	# Add params
+	if (@params)
+	{
+		for (my $i = 0; $i < scalar(@params); $i++)
+		{
+			my @value = split(/=/, $params[$i]);
+			$localdirs .= '/' . $value[1];
+		}
+	}
 
-        # Manually override the proposed destination
-        my $dest = edit_dest($tmp_dest);
+	# Recompose the destination path
+	my $tmp_dest = $fld . $localdirs;
 
-        return $dest;
+	# Manually override the proposed destination
+	my $dest = edit_dest($tmp_dest);
+
+	return $dest;
+}
+
+sub find_dest_host
+{
+	my $host = $_[0];
+	my $path = $_[1];
+
+	# Extract a path
+	my @dirs = split(/\//, $path);
+	my $localdirs = '/';
+	for (my $i = 1; $i < scalar(@dirs); $i++)
+	{
+		if ($dirs[$i] =~ m/[\*\?\[\]\{\}]/)
+		{
+			$localdirs .= '';
+		}
+		else
+		{
+			$localdirs .= $dirs[$i] . '/';
+		}
+	}
+
+	my $tmp_dest = $host . $localdirs;
+	my $dest = edit_dest($tmp_dest);
+
+	return $dest;
 }
 
 sub edit_dest
@@ -425,6 +561,65 @@ sub wget_recursive
         return @output;
 }
 
+sub rsync_simple
+{
+	my $source = $_[0];
+	my $dest = $_[1];
+	my $dirname = dirname($dest);
+	my @output = ();
+
+	unless(-d $dirname or make_path($dirname))
+	{
+		die("ERROR: cannot make path $dirname.\n");
+	}
+
+	open(RSYNCOUTPUT, "rsync -rtvz --out-format=\"%t %f\" $source $dest 2>&1 |");
+	while (my $line = <RSYNCOUTPUT>)
+	{
+		if ($line =~ m/^\d{4}\/\d{2}\/\d{2}/)
+		{
+			chomp($line);
+			$line .= " " . $dest . "\n";
+			my @line = split(" ", $line);
+			$line[2] = $source;
+			push @output, @line;
+		}
+		print $line;
+	}
+
+	return @output;
+}
+
+sub rsync_recursive
+{
+	my $source = $_[0];
+	my $dest = $_[1];
+	my $dirname = dirname($dest);
+	my @output = ();
+
+	unless(-d $dirname or make_path($dirname))
+	{
+		die("ERROR: cannot make path $dirname.\n");
+	}
+
+	open(RSYNCOUTPUT, "rsync -rtvz --out-format=\"%t %f\" $source $dest 2>&1 |");
+	while (my $line = <RSYNCOUTPUT>)
+	{
+		if ($line =~ m/^\d{4}\/\d{2}\/\d{2}/)
+		{
+			chomp($line);
+			$line .= " " . $dest . "\n";
+			my @line = split(" ", $line);
+			$line[3] = $line[3] . $line[2];
+			$line[2] = dirname($source) . "/" . $line[2];
+			push @output, @line;
+		}
+		print $line;
+	}
+
+	return @output;
+}
+
 sub update_index
 {
         my @log = @{$_[0]};
@@ -516,7 +711,7 @@ sub fix_index
 
         # Now check if the files with the same path have actually the 
         # same checksum
-        foreach $path (keys(%tree_bypath))
+        foreach my $path (keys(%tree_bypath))
         {
                 if (exists($index_bypath{$path}) and $tree_bypath{$path}[3] ne $index_bypath{$path}[3])
                 {
@@ -613,7 +808,7 @@ sub is_pseudo
 
 sub is_recursive
 {
-        my $path = $_[0];
+	my $path = $_[0];
 
-        return ($path =~ m/[\*\?\[\]\{\}]/);
+	return ($path =~ m/[\*\?\[\]\{\}]/);
 }
